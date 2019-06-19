2904e986e8107d5065eee7ab1563536db7b1c611
happyAnger6 <happyAnger6@163.com>
2019-06-19 19:57:52 +0800

    add

--- src/lib/anger_dlist_out.c
+++ src/lib/anger_dlist_out.c
@@ -32,3 +32,40 @@ int dlist_ins_next(DList *pList, DListElem *pElem, void *pData)
     return 0;
 
 }
+
+int dlist_ins_prev(DList *pList, DListElem *pElem, void *pData)
+{
+   if( 0!=dlist_size(pList) && pElem == NULL)
+       return -1;
+
+
+   pNewElem = (DListElem *)malloc(DlistElem);
+   if(NULL == pNewElem)
+       return -1;
+
+   pNewElem->pData = pData;
+   if(0 == dlist_size(pList))
+   {
+        pList->pHead = pNewElem;
+        pList->pTail = pNewElem
+        pNewElem->pPrev = NULL;
+        pNewElem->pNext = NULL;
+   } 
+   else
+   {
+       pNewElem->pPrev = pElem->pPrev;
+       pNewElem->pNext = pElem;
+       if(NULL == pElem->pPrev)
+       {
+            pNewElem->pPrev = NULL; 
+       }
+       else
+       {
+            pElem->pPrev->pNext = pNewElem ;
+       }
+       pElem->pPrev = pNewElem;
+
+   }
+   pList->iSize++;
+   return 0;
+}
